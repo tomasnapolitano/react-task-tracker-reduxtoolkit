414e76a022693495fa02905d9705c6e4db2f8c40
tomasnapolitano <t97napolitano@gmail.com>
2022-09-26 14:27:32 -0300

    added temporary render of task details

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,22 @@
 import { useSelector } from "react-redux";
 
 const TaskList = () => {
-
-    const state = useSelector((state) => state)
     const tasks = useSelector((state) => state.tasks)
     console.log("state in tasklist",tasks)
-    return(<h1>TaskList</h1>)
+    return(
+    <div>
+        <h1>Tasks: {tasks.length}</h1>
+        {tasks.map((task) =>{
+            console.log("task in map", task);
+            return(    // Este return no debería estar, ya que vamos a usar el componente Task. Por ahora lo hacemos asi ya que no tenemos Task definido.
+            <div key={task.id}>
+            <h2>{task.title}</h2>
+            <p>{task.description}</p>
+            </div>
+            )
+        })}
+    </div>
+    )
 }
 
 export default TaskList;
